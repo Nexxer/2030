a2e76643c84b6875babd4e7630807506011ed52b
Nexxer <yairopb@yandex.ru>
2021-08-19 09:54:05 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
     // Открытие меню мобильного
-$('.header__mobile-burger').on('click', function () {
+$('.header__mobile-burger').on('click', function (e) {
+    e.preventDefault();
     const menuMobile = $('.header__nav');
     menuMobile.slideToggle("normal");
 })
+
+    //переключение активного таба
+$('.result .tabs .tab').on('click', function (e) {
+    e.preventDefault();
+    const target = e.target;
+    $(target).siblings('.tab').removeClass('tab_active');
+    $(target).addClass('tab_active');
+});
 
 })
